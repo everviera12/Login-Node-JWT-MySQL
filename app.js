edabc711f5e624ca909f4180a6d073b6b3b7ee26
everviera12 <evertigre10@gmail.com>
2023-10-16 13:17:55 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express"); // Importamos el módulo 'express'
+const dotenv = require("dotenv");  // Importamos el módulo 'dotenv' para cargar variables de entorno
+const cookieParser = require("cookie-parser"); // Importamos el módulo 'cookieParser' para gestionar cookies en solicitudes HTTP.
+
+
+const app = express(); // Creamos una instancia de la aplicación Express.
+
+
+app.set("view engine", "ejs"); // Configuración del motor de vistas EJS
+
+
+app.use(express.static("public")); // Configuración para servir archivos estáticos desde la carpeta "public"
+
+
+// Middleware para analizar datos de formularios con codificación URL y JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+dotenv.config({ path: "./env/.env" }); // Carga variables de entorno desde el archivo
+
+
+// Middleware para gestionar cookies
+// app.use(cookieParser);
+
+
+app.use("/", require("./routers/login.router")); // Configuramos ruta principal para utilizar las rutas definidas en el archivo "login.router".
+
+
+// Inicia el servidor en el puerto 3000
+app.listen(3000, () => {
+  console.log("SERVER ON http://127.0.0.1:3000");
+});
+
